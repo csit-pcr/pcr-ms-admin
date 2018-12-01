c100482c1362b713d31320e8363e3a0ae94b8ab3
levi85 <joshua.zhangzy@gmail.com>
2018-12-01 16:24:34 +0800

    added /config/application.properties to .gitignore file

---
+++ src/main/java/sg/gov/csit/pcrmsadmin/PcrMsAdminApplication.java
@@ -0,0 +1,12 @@
+package sg.gov.csit.pcrmsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcrMsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcrMsAdminApplication.class, args);
+	}
+}
---
+++ src/test/java/sg/gov/csit/pcrmsadmin/PcrMsAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package sg.gov.csit.pcrmsadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PcrMsAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
